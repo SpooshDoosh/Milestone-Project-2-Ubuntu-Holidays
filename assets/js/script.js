61dad32d3db81e8726beea57f5f830ebea5d245d
SpooshDoosh <kelvinwnicholson@gmail.com>
2022-10-18 15:22:46 +0000

    Add google maps markers for activities in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,47 @@ function initMap() {
     center: { lat: -33.9249, lng: 18.4241 },
     zoom: 15,
   });
+
+  const activities = [
+    [
+        "Iziko Bo-Kaap Museum",
+        -33.92153422096886,
+        18.41498655573794
+    ],
+    [
+        "Two Oceans Aquarium"
+        -33.90796262809507, 
+        18.41761337016955
+
+    ],
+    [
+        "V&A Waterfront"
+        -33.90655943993641, 
+        18.419320084144353
+    ],
+    [
+        "Table Mountain National Park"
+        -33.96040795755455, 
+        18.401253107872943
+    ],
+    [
+        "Scootours"
+        -33.942850666607775, 
+        18.395574727234926
+    ],
+
+
+  ];
+
+  for(let i = 0; i < activities.length; i++) {
+    const currentActivity= activities[i];
+
+    const marker = new google.maps.Marker({
+        position: {lat: parseFloat(currentActivity[1]), lng: parseFloat(currentActivity[2])},
+        map,
+        animation: google.maps.Animation.DROP
+      });
+  }
 }
 
   
