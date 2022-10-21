c91f8e04522a4a2ac261ac1bb8cbcfba40a0861a
SpooshDoosh <kelvinwnicholson@gmail.com>
2022-10-21 09:48:01 +0000

    Add infowindows to markers, add links to infowindows

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,192 +41,238 @@ function initMap() {
   const locations = [{
       position: new google.maps.LatLng(-33.93457336048657, 18.44743738570852),
       type: "lodging",
-      title: "Garden Court Nelson Mandela Boulevard"
+      title: "Garden Court Nelson Mandela Boulevard",
+      url: '<br><a href="https://g.page/Garden-Court-Nelson-Mandela-Blvd?share" target="_blank">Check it out!</a>'
     },
     {
       position: new google.maps.LatLng(-33.932077596395125, 18.427120298079892),
       type: "lodging",
-      title: "Best Western Cape Suites Hotel"
+      title: "Best Western Cape Suites Hotel",
+      url: '<br><a href="https://goo.gl/maps/4FibyyCjfTMs6wF68" target="_blank">Check it out!</a>'
     },
     {
       position: new google.maps.LatLng(-33.93946172596997, 18.405109752022675),
       type: "lodging",
-      title: "Kensington Place"
+      title: "Kensington Place",
+      url: '<br><a href="https://goo.gl/maps/ySroDGH3isgK5jrr9" target="_blank">Check it out!</a>'
     },
     {
       position: new google.maps.LatLng(-33.9290353767041, 18.37786465477859),
       type: "lodging",
-      title: "Ellerman House"
+      title: "Ellerman House",
+      url: '<br><a href="https://goo.gl/maps/scr44wxB9eC5fFL29" target="_blank">Check it out!</a>'
     },
     {
       position: new google.maps.LatLng(-33.952573869260945, 18.378261047523747),
       type: "lodging",
-      title: "The Bay Hotel"
+      title: "The Bay Hotel",
+      url: '<br><a href="https://g.page/TheBayHotelCapeTown?share" target="_blank">Check it out!</a>'
     },
     {
       position: new google.maps.LatLng(-33.95307830542599, 18.377451958483473),
       type: "lodging",
-      title: "The Marly Boutique Hotel and Spa"
+      title: "The Marly Boutique Hotel and Spa",
+      url: '<br><a href="https://goo.gl/maps/gLeyPZGJsJu4YMcE6" target="_blank">Check it out!</a>'
     },
     {
       position: new google.maps.LatLng(-33.921534588835705, 18.414986393558436),
       type: "attraction",
-      title: "Iziko Bo-Kaap Museam"
+      title: "Iziko Bo-Kaap Museam",
+      url: '<br><a href="https://goo.gl/maps/Lc5nSpWdANR8wWfd9" target="_blank">Check it out!</a>'
     },
     {
       position: new google.maps.LatLng(-33.90796315079628, 18.417612754114494),
       type: "attraction",
-      title: "Two Oceans Aquarium"
+      title: "Two Oceans Aquarium",
+      url: '<br><a href="https://g.page/2oceansaquarium?share" target="_blank">Check it out!</a>'
     },
     {
       position: new google.maps.LatLng(-33.90655963477768, 18.41932070406976),
       type: "attraction",
-      title: "V&A Waterfront"
+      title: "V&A Waterfront",
+      url: '<br><a href="https://goo.gl/maps/5LvBnccxSY4iPo358" target="_blank">Check it out!</a>'
     },
     {
       position: new google.maps.LatLng(-33.96041394155216, 18.401251959185316),
       type: "attraction",
-      title: "Table Mountain National Park"
+      title: "Table Mountain National Park",
+      url: '<br><a href="https://goo.gl/maps/rFUFmEefhtTQhqxk9" target="_blank">Check it out!</a>'
     },
     {
       position: new google.maps.LatLng(-33.942850502945646, 18.395574742422603),
       type: "attraction",
-      title: "Scootours Cape Town"
+      title: "Scootours Cape Town",
+      url: '<br><a href="https://goo.gl/maps/NiJ14nBjFWhHQnGW8" target="_blank">Check it out!</a>'
     },
     {
       position: new google.maps.LatLng(-33.92849433315896, 18.414953038785907),
       type: "attraction",
-      title: "The Planetarium"
+      title: "The Planetarium",
+      url: '<br><a href="https://goo.gl/maps/wi4p1qX8xQHrwrpc6" target="_blank">Check it out!</a>'
     },
     {
       position: new google.maps.LatLng(-33.95079890743787, 18.37856462600259),
       type: "restaurant",
-      title: "The 41"
+      title: "The 41",
+      url: '<br><a href="https://goo.gl/maps/zTRmYYjnbsKzBthF7" target="_blank">Check it out!</a>'
     },
     {
       position: new google.maps.LatLng(-33.95420487864492, 18.377293247291995),
       type: "restaurant",
-      title: "Dizzy's Restaurant & Pub"
+      title: "Dizzy's Restaurant & Pub",
+      url: '<br><a href="https://g.page/dizzys-restaurant-pub-camps-bay?share" target="_blank">Check it out!</a>'
     },
     {
       position: new google.maps.LatLng(-33.952941552485946, 18.377413193092444),
       type: "restaurant",
-      title: "Zenzero"
+      title: "Zenzero",
+      url: '<br><a href="https://goo.gl/maps/XYuvigMomR8jFQA66" target="_blank">Check it out!</a>'
     },
     {
       position: new google.maps.LatLng(-33.952880268214166, 18.377543748266113),
       type: "restaurant",
-      title: "Surfshack"
+      title: "Surfshack",
+      url: '<br><a href="https://g.page/surfshackdiner?share" target="_blank">Check it out!</a>'
     },
     {
       position: new google.maps.LatLng(-33.95331149967668, 18.37730078417984),
       type: "restaurant",
-      title: "The Raj"
+      title: "The Raj",
+      url: '<br><a href="https://goo.gl/maps/ceX4sVuBvePTFw9FA" target="_blank">Check it out!</a>'
     },
     {
       position: new google.maps.LatLng(-33.95066990407833, 18.378643586376814),
       type: "restaurant",
-      title: "Café Caprice"
+      title: "Café Caprice",
+      url: '<br><a href="https://g.page/cafecapriceCT?share" target="_blank">Check it out!</a>'
     },
     {
       position: new google.maps.LatLng(-29.84018750698891, 31.03486056708485),
       type: "lodging",
-      title: "Blue Waters Hotel"
+      title: "Blue Waters Hotel",
+      url: '<br><a href="https://goo.gl/maps/6ZBGjQasoKAEs9FM8" target="_blank">Check it out!</a>'
     },
     {
       position: new google.maps.LatLng(-29.767888029678495, 31.040196127070068),
       type: "lodging",
-      title: "Endless Horizons Boutique Hotel"
+      title: "Endless Horizons Boutique Hotel",
+      url: '<br><a href="https://g.page/EndlessHorizonsHotel?share" target="_blank">Check it out!</a>'
     },
     {
       position: new google.maps.LatLng(-29.871799905227, 31.051494610052373),
       type: "lodging",
-      title: "Point Waterfront Apartments"
+      title: "Point Waterfront Apartments",
+      url: '<br><a href="https://goo.gl/maps/JuTw8G3QverDsZid6" target="_blank">Check it out!</a>'
     },
     {
       position: new google.maps.LatLng(-29.72742600280119, 31.08776482719978),
       type: "lodging",
-      title: "Beverly Hills Hotel"
+      title: "Beverly Hills Hotel",
+      url: '<br><a href="https://g.page/beverly-hills-hotel?share" target="_blank">Check it out!</a>'
     },
     {
       position: new google.maps.LatLng(-29.76211511736365, 31.06450694481901),
       type: "lodging",
-      title: "Bentley On The Beach"
+      title: "Bentley On The Beach",
+      url: '<br><a href="https://goo.gl/maps/tnFALsJjkBXkBHCp9" target="_blank">Check it out!</a>'
     },
     {
       position: new google.maps.LatLng(-29.722891290883865, 31.07321000005819),
       type: "lodging",
-      title: "Premier Hotel Umhlanga"
+      title: "Premier Hotel Umhlanga",
+      url: '<br><a href="https://goo.gl/maps/9qZt6GgezJYRZkTV8" target="_blank">Check it out!</a>'
     },
     {
       position: new google.maps.LatLng(-29.867256143393877, 31.045852022914712),
       type: "attraction",
-      title: "uShaka Marine World"
+      title: "uShaka Marine World",
+      url: '<br><a href="https://g.page/uShakaFanPage?share" target="_blank">Check it out!</a>'
     },
     {
       position: new google.maps.LatLng(-29.848254350424348, 31.008287327698643),
       type: "attraction",
-      title: "Durban Botanic Gardens"
+      title: "Durban Botanic Gardens",
+      url: '<br><a href="https://g.page/DurbanBotanicGardens?share" target="_blank">Check it out!</a>'
     },
     {
       position: new google.maps.LatLng(-29.868968500400943, 31.04714885733236),
       type: "attraction",
-      title: "Zulumoon Gondolas Boat Rides"
+      title: "Zulumoon Gondolas Boat Rides",
+      url: '<br><a href="https://g.page/ZulumoonGondolas?share" target="_blank">Check it out!</a>'
     },
     {
       position: new google.maps.LatLng(-29.87536836115765, 30.989191249864753),
       type: "attraction",
-      title: "Phansi Museum"
+      title: "Phansi Museum",
+      url: '<br><a href="https://goo.gl/maps/xA3mWxvt8ohmWtf9A" target="_blank">Check it out!</a>'
     },
     {
       position: new google.maps.LatLng(-29.835307625261567, 31.034989998793606),
       type: "attraction",
-      title: "Suncoast Casino, Hotels And Entertainment"
+      title: "Suncoast Casino, Hotels And Entertainment",
+      url: '<br><a href="https://g.page/gottalovesuncoast?share" target="_blank">Check it out!</a>'
     },
     {
       position: new google.maps.LatLng(-29.82812161837584, 31.030668991933982),
       type: "attraction",
-      title: "I Heart Market"
+      title: "I Heart Market",
+      url: '<br><a href="https://goo.gl/maps/R6R8WzMiNhWVwqAZ8" target="_blank">Check it out!</a>'
     },
     {
       position: new google.maps.LatLng(-29.825881555509163, 31.02251484943805),
       type: "restaurant",
-      title: "Mozambik Morningside"
+      title: "Mozambik Morningside",
+      url: '<br><a href="https://goo.gl/maps/CUWzmE2vyRuE17si6" target="_blank">Check it out!</a>'
     },
     {
       position: new google.maps.LatLng(-29.832025174406677, 31.022794139675547),
       type: "restaurant",
-      title: "Durban Curry House"
+      title: "Durban Curry House",
+      url: '<br><a href="https://goo.gl/maps/16isvnNxqRRaQULU9" target="_blank">Check it out!</a>'
     },
     {
       position: new google.maps.LatLng(-29.85953517053618, 30.998608554175686),
       type: "restaurant",
-      title: "Amsterdam Bar & Grill"
+      title: "Amsterdam Bar & Grill",
+      url: '<br><a href="https://goo.gl/maps/pVR7sx7mZg6Z9rBH7" target="_blank">Check it out!</a>'
     },
     {
       position: new google.maps.LatLng(-29.859477235223967, 30.998117485508292),
       type: "restaurant",
-      title: "Olive & Oil Glenwood"
+      title: "Olive & Oil Glenwood",
+      url: '<br><a href="https://goo.gl/maps/EAB3qhQLWcZeV9qN7" target="_blank">Check it out!</a>'
     },
     {
       position: new google.maps.LatLng(-29.858881456862335, 30.99713025152184),
       type: "restaurant",
-      title: "Julio's Continental Restaurant"
+      title: "Julio's Continental Restaurant",
+      url: '<br><a href="https://goo.gl/maps/6Fp55SP57zhCSCNN9" target="_blank">Check it out!</a>'
     },
     {
       position: new google.maps.LatLng(-29.82746442181384, 31.010949407689445),
       type: "restaurant",
-      title: "Spiga"
+      title: "Spiga",
+      url: '<br><a href="https://goo.gl/maps/23CUb1LQVoqhbiwX7" target="_blank">Check it out!</a>',
     }
   ];
 
-  locations.forEach((location) => {
-    new google.maps.Marker({
-      position: location.position,
-      icon: icons[location.type].icon,
+  for (let i = 0; i < locations.length; i++) {
+
+    const marker = new google.maps.Marker({
+      position: locations[i].position,
+      title: locations[i].title,
+      icon: icons[locations[i].type].icon,
       map: map,
     });
-  });
+    //Create infowindows with location titles and links//
+    const infowindow = new google.maps.InfoWindow({
+      content: locations[i].title + locations[i].url,
+    });
+
+    marker.addListener("click", () => {
+      infowindow.open(map, marker);
+    });
+  }
 
   //Create divs for legend icons and populate with gathered info//
 
